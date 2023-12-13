dc3ba35cce84cf917961d471c18b061ee4779cb2
lutfiHidayanto <hajidlutfi@gmail.com>
2023-12-13 13:20:03 +0700

    edit index

--- createindex.sql
+++ createindex.sql
@@ -1,9 +1,6 @@
 CREATE NONCLUSTERED INDEX [idx_non_id_orang] 
 ON [ORANG] ([Id_orang]);
 
-CREATE CLUSTERED INDEX [idx_clustered_namaOrang] 
-ON [ORANG] ([Nama_depan]);
-
 CREATE NONCLUSTERED INDEX [idx_non_alamat] 
 ON [ALAMAT] ([Id_alamat]);
 
