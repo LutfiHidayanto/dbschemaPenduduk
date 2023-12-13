b60d36af533dccb02ac81c271325278607bb67d4
lutfiHidayanto <hajidlutfi@gmail.com>
2023-12-13 13:15:39 +0700

    mengurangi index

--- createindex.sql
+++ createindex.sql
@@ -2,7 +2,7 @@ CREATE NONCLUSTERED INDEX [idx_non_id_orang]
 ON [ORANG] ([Id_orang]);
 
 CREATE CLUSTERED INDEX [idx_clustered_namaOrang] 
-ON [ORANG] ([Nama_depan], [Nama_tengah], [Nama_belakang]);
+ON [ORANG] ([Nama_depan]);
 
 CREATE NONCLUSTERED INDEX [idx_non_alamat] 
 ON [ALAMAT] ([Id_alamat]);
